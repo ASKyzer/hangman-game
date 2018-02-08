b4039fce53ad37f314fb3124c8aa9669f0566f2c
Adrian Kyzer <ASKyzer@github.com>
2018-02-07 20:51:20 -0500

    created a different script.js to start over.  added used-letter class after just presses that key. some global variable and the array of cities.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,85 +1,101 @@
-
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// array of American cities
+// array of cities in our category
 var cities = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
 
+// number of guess to started
 var lives = 9;
-var score = 0;
+// score at beginning
+var wins = 0;
 
-var randomCityIndex = Math.floor(Math.random() * cities.length -1);
+// key event to listen  for the letters that the players doctype
+document.onkeyup = function(event){
 
-// console.log(randomCity);
-// logs the randomly generated city
-chosenCity = cities[randomCityIndex];
-// console.log(chosenCity);
-// var currentWord = chosenCity.split('');
-// console.log(currentWord);
+var userInput = String.fromCharCode(event.which).toLowerCase();
+console.log(userInput);
+
+// takes the letter user presses and adds a class that makes the letter gray signifying it's been used
+  if (userInput === "a") {
+    $("#btn-a").addClass("used-letter");
+  }
+  if (userInput === "b") {
+    $("#btn-b").addClass("used-letter");
+  }
+  if (userInput === "c") {
+    $("#btn-c").addClass("used-letter");
+  }
+  if (userInput === "d") {
+    $("#btn-d").addClass("used-letter");
+  }
+  if (userInput === "e") {
+    $("#btn-e").addClass("used-letter");
+  }
+  if (userInput === "f") {
+    $("#btn-f").addClass("used-letter");
+  }
+  if (userInput === "g") {
+    $("#btn-g").addClass("used-letter");
+  }
+  if (userInput === "h") {
+    $("#btn-h").addClass("used-letter");
+  }
+  if (userInput === "i") {
+    $("#btn-i").addClass("used-letter");
+  }
+  if (userInput === "j") {
+    $("#btn-j").addClass("used-letter");
+  }
+  if (userInput === "k") {
+    $("#btn-k").addClass("used-letter");
+  }
+  if (userInput === "l") {
+    $("#btn-l").addClass("used-letter");
+  }
+  if (userInput === "m") {
+    $("#btn-m").addClass("used-letter");
+  }
+  if (userInput === "n") {
+    $("#btn-n").addClass("used-letter");
+  }
+  if (userInput === "o") {
+    $("#btn-o").addClass("used-letter");
+  }
+  if (userInput === "p") {
+    $("#btn-p").addClass("used-letter");
+  }
+  if (userInput === "q") {
+    $("#btn-q").addClass("used-letter");
+  }
+  if (userInput === "r") {
+    $("#btn-r").addClass("used-letter");
+  }
+  if (userInput === "s") {
+    $("#btn-s").addClass("used-letter");
+  }
+  if (userInput === "t") {
+    $("#btn-t").addClass("used-letter");
+  }
+  if (userInput === "u") {
+    $("#btn-u").addClass("used-letter");
+  }
+  if (userInput === "v") {
+    $("#btn-v").addClass("used-letter");
+  }
+  if (userInput === "w") {
+    $("#btn-w").addClass("used-letter");
+  }
+  if (userInput === "x") {
+    $("#btn-x").addClass("used-letter");
+  }
+  if (userInput === "y") {
+    $("#btn-y").addClass("used-letter");
+  }
+  if (userInput === "z") {
+    $("#btn-z").addClass("used-letter");
+  }
+
+};
+
 //
-// for (i = 0; i < currentWord.length; i++){
-// };
-console.log(randomCityIndex);
-console.log(chosenCity);
 
-var word = chosenCity;
-var wordLength = word.length;
-// var underscores = "";
-var result = [];
-for (i=0; i<wordLength; i++) {
-  result.push("_")
-}
-
-result.toString();
-
-console.log(result[0]);
-console.log(result[1]);
-console.log(result[2]);
-console.log(result[3]);
-console.log(result[4]);
-console.log(result[5]);
-
-for(var i = 0; i < wordLength; i++) {
-  document.write(result[i] + '  ');
-}
-console.log(result);
-// var answer="";
-// for (var i=0; i < currentWord.length; i++ ){
-//     answer = answer +'- ';
-//   };
-//     console.log(answer);
-//
-// var displayHiddenWord =
-//     "<h4>" + answer + "</h4";
-//
-// document.getElementById('word').innerHTML = displayHiddenWord;
-//
-// document.onkeyup = function(event) {
-//     // Determines which key was pressed.
-//     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-    // var result = function () {
-    // var wordHolder = document.getElementById('used');
-    // var correct = document.createElement('ul');
-    //
-    // for (var i = 0; i < currentWord.length; i++) {
-    //   correct.setAttribute('id', 'my-word');
-    //   var guess = document.createElement('li');
-    //   guess.setAttribute('class', 'guess');
-    //   if (currentWord[i] === answer[i]) {
-    //     guess.innerHTML = "-";
-    //     space = 1;
-    //   } else {
-    //     guess.innerHTML = "_";
-    //   }
-    //
-    //   geusses.push(guess);
-    //   wordHolder.appendChild(correct);
-    //   correct.appendChild(guess);
-    // }
-
-// }
-
-
-
-
-// };
+// at game end, the game generates another word
+  // meaning call the funtion to get new word
+    // getNewWord(); ???
